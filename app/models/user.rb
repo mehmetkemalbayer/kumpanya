1ef2b342ed1183354f860f034fa581867de427d7
mehmetkemalbayer <kemal.bayer@gmail.com>
2016-05-12 16:12:40 +0300

    adds turkish to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_many :ucampaigns, through: :user_campaigns
+	has_many :campaigns, through: :user_campaigns
 end
