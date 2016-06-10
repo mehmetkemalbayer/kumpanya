f01b76a9b7164bb6f5017351295bd6ea1f3570be
mehmetkemalbayer <kemal.bayer@gmail.com>
2016-06-10 17:24:11 +0300

    fixes user_campaign_products problem

--- app/controllers/user_campaign_products_controller.rb
+++ app/controllers/user_campaign_products_controller.rb
@@ -1,3 +1,15 @@
 class UserCampaignProductsController < ApplicationController
-
+    def create
+        @userCampaignProduct = UserCampaignProduct.new
+        @userCampaignProduct.user_campaign_id = params[:id]
+        @userCampaignProduct.product_id = params[:product_id]
+        @userCampaignProduct.save
+        redirect_to UserCampaign.find(params[:id]).campaign
+    end
+    
+    private
+    def userCampaignProductParams
+        params.permit(:id, :product_id)
+    end
+    
 end
